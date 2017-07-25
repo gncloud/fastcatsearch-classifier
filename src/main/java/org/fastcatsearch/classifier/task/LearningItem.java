f33c62a7976d8ca76af23b2c413ea50e0edd0fec
lupfeliz <lupfeliz@gmail.com>
2017-07-25 16:52:44 +0900

    add maven pom file LearningTask modified

---
+++ src/main/java/org/fastcatsearch/classifier/task/LearningItem.java
@@ -0,0 +1,20 @@
+package org.fastcatsearch.classifier.task;
+
+public class LearningItem {
+	private int category;
+	private String text;
+	
+	public int getCategory() {
+		return category;
+	}
+	public void setCategory(int category) {
+		this.category = category;
+	}
+	public String getText() {
+		return text;
+	}
+	public void setText(String text) {
+		this.text = text;
+	}
+
+}
